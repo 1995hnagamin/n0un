b694d2bab951a4eae01a309c618e78ed5ffb5d7a
Hideaki Nagamine <hnagamin@kmc.gr.jp>
2015-09-20 21:27:16 +0900

    classify perser tests

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -7,12 +7,13 @@ let parse_result_test program expr =
   ("test " ^ program) >:: 
     (fun test_ctxt -> assert_equal (parse program) expr)
 
-let parser_tests =
+let parse_tests =
   List.map (fun (x, y) -> parse_result_test x y) [
     "10;;", Int 10;
     "zero;;", Zero;
     "succ;;", Succ;
     "@1/10;;", Proj(1, 10);
+    
     "succ[zero];;", Comp(Succ, [Zero]);
     "@1/4[@1/3, @2/3, @3/3, @1/3];;", 
       Comp(Proj(1,4), [Proj(1,3); Proj(2,3); Proj(3,3); Proj(1,3)]);
@@ -24,14 +25,29 @@ let parser_tests =
            [App (Zero, []);
             App (Succ, [Int 0]);
             App (Proj(1,2), [Int 2; Int 3])]);
-    "@1/2->zero(0);;", PRec(Proj(1,2), App(Zero, [Int 0]));
-    "(@1/2->zero)(0);;", App(PRec(Proj(1,2), Zero), [Int 0]);
-    "succ[zero](0);;", App(Comp(Succ, [Zero]), [Int 0]);
-    "@1/3 -> @2/2 -> zero;;",
-      PRec(Proj(1,3), PRec(Proj(2,2), Zero));
-    "@1/4 -> @1/2[zero, zero];;",
-      PRec(Proj(1,4), Comp(Proj(1,2), [Zero; Zero]));
-]
+  ]
+;;
+
+let prog_eql_test prog1 prog2 =
+  prog1 ^ " = " ^ prog2 >::
+    (fun test_ctxt -> assert_equal (parse prog1) (parse prog2))
+;;
+
+let precedence_tests =
+  List.map (fun (s, t) -> prog_eql_test s t) [
+    "succ[zero](0);;",          "(succ[zero]) (0);;";
+    "@1/2->zero(0);;",          "@1/2 -> (zero(0));;";
+    "@1/2->@1/2[zero,zero];;",  "@1/2 -> (@1/2[zero,zero]);;";
+  ]
+;;
+
+let associativity_tests =
+  List.map (fun (s, t) -> prog_eql_test s t) [
+    "@1/3->@2/2->zero;;", "@1/3 -> (@2/2 -> zero);;";
+  ]
+;;
+
+let parser_tests = parse_tests @ precedence_tests @ associativity_tests
 
 let suite = "test parser" >::: parser_tests
 
