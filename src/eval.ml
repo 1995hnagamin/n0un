71a7a7fc918e7b7a5e06f928486a58a188698d37
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-09-21 17:31:41 +0900

    eval variable on eval

--- src/eval.ml
+++ src/eval.ml
@@ -3,32 +3,38 @@ open Util
 
 exception Runtime_error of string
 
-let apply_succ = function
+let rec apply_succ env = function
   Int n -> Int (n + 1)
+| Var x ->
+    let x = Environment.lookup x env in
+    apply_succ env x
 | _ -> raise (Runtime_error "Only integer succeeds")
 
-let rec apply f xs = 
+let rec apply env f xs = 
   match f with
   Zero -> Int 0
-| Succ -> apply_succ (List.hd xs)
+| Succ -> apply_succ env (List.hd xs)
 | Proj(n, _) -> List.nth xs (n - 1)
 | Comp(g, fs) ->
-    let xs = List.map (fun f -> apply f xs) fs in
-    apply g xs
-| PRec(g, f) -> apply_prec g f (List.rev xs)
+    let xs = List.map (fun f -> apply env f xs) fs in
+    apply env g xs
+| PRec(g, f) -> apply_prec env g f (List.rev xs)
+| Var id ->
+    let f = Environment.lookup id env in
+    apply env f xs
 | _ -> raise (Runtime_error "Non-funtion object is applied")
 
-and apply_prec g f = function
-  (Int 0)::xs -> apply f (List.rev xs)
+and apply_prec env g f = function
+  (Int 0)::xs -> apply env f (List.rev xs)
 | (Int n)::xs ->
     let y = Int (n - 1) in
-    let z = apply_prec g f (y::xs) in
-    apply g (List.rev (z::y::xs))
+    let z = apply_prec env g f (y::xs) in
+    apply env g (List.rev (z::y::xs))
 | []  -> raise (Runtime_error "Arguments exhausted")
 | _   -> raise (Runtime_error "Wrong arguments")
 
 let rec eval env = function
-  App(f, xs) -> apply (eval env f) (List.map (eval env) xs)
+  App(f, xs) -> apply env (eval env f) (List.map (eval env) xs)
 | LetExp(x, v, body) ->
     let v = eval env v in
     let env = Environment.extend x v env in
