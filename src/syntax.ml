e6762751148cf256b621d8e9db84cf93fe17ee58
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-09-21 11:59:42 +0900

    add eval

--- src/syntax.ml
+++ src/syntax.ml
@@ -9,6 +9,21 @@ type exp =
 | Comp of exp * (exp list)
 | PRec of exp * exp
 
+let rec string_of_exp = function
+  Int n -> string_of_int n
+| App (f, xs) ->
+    let f = string_of_exp f
+    and xs = List.map string_of_exp xs in
+    f ^ "(" ^ String.concat "," xs ^ ")"
+| Zero -> "zero"
+| Succ -> "succ"
+| Proj(x, y) -> "@" ^ string_of_int x ^ "/" ^ string_of_int y
+| Comp(g, fs) ->
+    let g = string_of_exp g
+    and fs = List.map string_of_exp fs in
+    g ^ "[" ^ String.concat "," fs ^ "]"
+| PRec(g, f) -> string_of_exp g ^ "->" ^ string_of_exp f
+
 type program = exp
 ;;
 
