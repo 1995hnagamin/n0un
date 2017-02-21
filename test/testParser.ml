64e8d314b5d228ac6558a7a4c1120f0dd49f0200
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-02-21 22:51:14 +0900

    Add more test cases for variadic functions to testParser

--- test/testParser.ml
+++ test/testParser.ml
@@ -27,12 +27,19 @@ let parse_tests =
     "succ[zero]", Comp((Var "succ"), [(Var "zero")]);
     "@1/4[@1/3, @2/3, @3/3, @1/3]", 
       Comp(proj 1 4, [proj 1 3; proj 2 3; proj 3 3; proj 1 3]);
+    "@2[@3->add, @2/2, @1]",
+      Comp(proj_variadic 2,
+        [PRec(proj_variadic 3, (Var "add"));
+         proj 2 2;
+         proj_variadic 1]);
     "succ.zero", Comp((Var "succ"), [(Var "zero")]);
     "@1/2 -> zero", PRec(proj 1 2, (Var "zero"));
     "@1 -> zero", PRec(proj_variadic 1, (Var "zero"));
+    "func -> @4", PRec((Var "func"), proj_variadic 4);
     "zero()", App((Var "zero"), []);
     "succ(0)", App((Var "succ"), [Int 0]);
     "@2/3(1, 2, 3)", App(proj 2 3, [Int 1; Int 2; Int 3]);
+    "@3(1, 2, 3, 4)", App(proj_variadic 3, [Int 1; Int 2; Int 3; Int 4]);
     "@1/3(zero(), succ(0), @1(2, 3))",
       App (proj 1 3,
            [App ((Var "zero"), []);
@@ -43,6 +50,10 @@ let parse_tests =
       LetExp("f", (Var "succ"),
       LetExp("x", app (Var "succ") 42,
       App(Var "f", [Var "x"])));
+    "Let g = @1 In @1[g,g](1,2,3)",
+      LetExp("g", proj_variadic 1,
+      App(Comp(proj_variadic 1, [Var "g"; Var "g"]),
+          [Int 1; Int 2; Int 3]));
   ]
 
 
